8a1c3c5ea0888b313192cc0401f3eac77a130dad
MainLaptop <40152651+Quivun@users.noreply.github.com>
2022-09-16 10:41:21 -0500

    Test 2 Ready
    Overhead implementation is accounted for and needs minor tweaks. Steps are also defined with note that sine wave should be in powers of 4 for balanced representation.

--- Lab2/3Waveforms.py
+++ Lab2/3Waveforms.py
@@ -1,3 +1,6 @@
+from math import sin, pi
+from time import sleep
+from busio import I2C
 import board
 import adafruit_mcp4725
 import RPi.GPIO as GPIO
@@ -5,11 +8,8 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-from busio import I2C
-from time import sleep
-from math import sin,pi
-
-btn = 4
+# dac.value[0,65525], dac.raw_value[0,4095], dac.normalized_value[0,1.0](Floating Point)
+# "If you need the most precise output use the raw_output value for setting voltage." - Adafruit
 
 """
 
@@ -23,20 +23,33 @@ Vout is analog output -> To oscilloscope [0,3.3]V
 
 """
 
-# Globals delta,waveform,frequency,voltageMax
+btn = 4
+funcDict = {"1": waveformSquare, "2": waveformTriangle, "3": waveformSin}
+i2c = I2C(board.SCL, board.SDA)  # Initialize I2C bus.
 
-funcDict = {"1" : waveformSquare, "2" : waveformTriangle, "3" : waveformSin}
+#Globals
+dac = adafruit_mcp4725.MCP4725(i2c)  # Initialize MCP4725.
+delta = False
+waveform = "NULL"
+frequency = 0
+voltageMax = 0.01
+absMax = 3.3
 
 def setup():
-    GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
+    GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+def changeCheck():
+    return delta
 
 def btnPress():
     global delta
-    if (delta):
-        delta = False
     global waveform
     global frequency
     global voltageMax
+    global absMax
+    if (delta):
+        delta = False
+    sleep(.25)
     waveform = input("Enter Waveform : 1 for Square, 2 for Triangle, and 3 for Sin : ")
     frequency = int(input("Enter, in hertz, the frequency of the wave : "))
     voltageMax = float(input("Input maximum voltage [Less than or equal to 3.3V, will default to [0,3.3] if beyond either bounds : "))
@@ -44,53 +57,55 @@ def btnPress():
     return (funcDict[waveform]())
 
 def waveformSquare():
-    global delta
-    global dac
     halfCycle = 1.0/(2.0*frequency)
-    while delta:
-        dac.set_voltage(int(voltageMax))
+    while changeCheck():
+        dac.set_voltage = int(4095.0*voltageMax/absMax)
         sleep(halfCycle)
-        dac.set_voltage(int(voltageMax))
+        dac.set_voltage = 0
+        sleep(halfCycle)
     return
 
 def waveformTriangle():
-    global dac
     halfCycle = 1.0/(2.0*frequency)
-    maxVal = int(4095.0*voltageMax/3.3)
-    tStep = halfCycle/maxVal
-    while delta:
-        for i in range(0,maxVal+1,1):
-            dac.raw_value = i
-            sleep(tStep)
-        for i in range(maxVal, -1, -1):
-            dac.raw_value = i
-            sleep(tStep)
+    maxVal = int(4095.0*voltageMax/absMax)
+    stepAmt = 29
+    increment = maxVal//stepAmt
+    tStep = halfCycle//stepAmt
+    overHeadConstant = 0.0003
+    if (tStep < overHeadConstant):
+        while changeCheck():
+            for i in range(0, maxVal+1, increment):
+                dac.raw_value = i
+            for i in range(stepAmt*maxVal, -1, increment):
+                dac.raw_value = i
+    else:
+        while delta:
+            for i in range(0, maxVal+1, increment):
+                dac.raw_value = i
+                sleep(tStep - overHeadConstant)
+            for i in range(stepAmt*maxVal, -1, increment):
+                dac.raw_value = i
+                sleep(tStep - overHeadConstant)
     return
-        
+
 
 def waveformSin():
-    global dac
-    t = 0.0
     fullCycle = 1.0/frequency
-    maxVal = int(4095.0*voltageMax/3.3)
+    maxVal = int(4095.0*voltageMax/absMax)
     tStep = fullCycle/maxVal
-    while delta:
-        if (t > 1):
-            t-=1
-        dac.raw_value(int((maxVal*sin(pi*2*t)+1)/2.0))
-        t += tStep
-        sleep(tStep)
+    stepAmt = 16 # Powers of 4 should be the step amount for balanced demonstration
+    while changeCheck():
+        for i in range(0,stepAmt+1,1):
+            dac.raw_value(int(maxVal*(sin(pi*2*i/float(stepAmt))+1)/2.0))
     return
-    
+
+
 def main():
-    global dac
     setup()
-    i2c = I2C(board.SCL, board.SDA) # Initialize I2C bus.
-    dac = adafruit_mcp4725.MCP4725(i2c) # Initialize MCP4725.
-    # dac.value[0,65525], dac.raw_value[0,4095], dac.normalized_value[0,1.0](Floating Point)
-    # "If you need the most precise output use the raw_output value for setting voltage." - Adafruit
     dac.raw_value = 0
     GPIO.add_event_detect(btn, GPIO.FALLING, callback=btnPress)
+    while(True):
+        pass
 
 
 if __name__ == "__main__":
