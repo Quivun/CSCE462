25f6899821ca02233c41ff7089e9deb151621b38
MainLaptop <40152651+Quivun@users.noreply.github.com>
2022-09-20 17:02:42 -0500

    Update AnalogToDigitalConverterWaveformDetector.py

--- Lab3/AnalogToDigitalConverterWaveformDetector.py
+++ Lab3/AnalogToDigitalConverterWaveformDetector.py
@@ -1,5 +1,6 @@
 from math import sin, pi
 from time import sleep
+import wave
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
 #import os
@@ -15,24 +16,36 @@ dac.raw_value = [0,4095]
 Channel.value = [0,(2^16)-1] Integer
 Channel.voltage = [0,3.3 (Or 5.0 maybe?)] Float 
 //This shall be our reference within data.
+//Will create 3 functions for sine triangle and square if needed.
 '''
 
 # Setup
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI) # create spi bus
 cs = digitalio.DigitalInOut(board.D22) # create chip select
 mcp = MCP.MCP3008(spi, cs) # create mcp object
-chan0 = AnalogIn(mcp, MCP.P0) # create analog input channel on pin 0
+channel = AnalogIn(mcp, MCP.P0) # create analog input channel on pin 0
 
-print('Raw ADC Value: ', chan0.value)
+print('Raw ADC Value: ', channel.value)
 
 # Hardware dependent values
 rPSInt = 1024 # Integer reads per second
 iInt = 1 # Float time before next check
-def s1DataGen():
-    ret = {}
-    return ret
+tolInt = 0 # Integer tolerance value 
 
-def s2Eval():
+def s1DataGen():
+    waveTotal = []
+    for i in range(0,rPSInt+1,1):
+        waveTotal.append(channel.value)
+    return waveTotal
+
+def s2DataClean(waveTotal):
+    # waveTotal is all data points gathered within an entire second of an unknown waveform
+    freqInt = 0
+    waveArr = []
+    return freqInt,waveArr
+
+def s3Eval(waveArr):
+    # waveArr is a subsection of the array containing a single full cycle of an unknown waveform.
     ret = "ans"
     return ret
 
@@ -40,9 +53,12 @@ def main():
     curWave = ""
     globalWave = ""
     while True:
-        freqInt , waveArr = s1DataGen()
+        waveTotal = s1DataGen()
+        
+        freqInt , waveArr = s2DataClean(waveTotal)
         print("Frequency : ",  freqInt)
-        curWave = s2Eval(waveArr)
+
+        curWave = s3Eval(waveArr)
         if (globalWave != curWave):
             globalWave = curWave
             print("New Waveform detected : ", globalWave)
